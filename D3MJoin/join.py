d20bf6d1ca002b5815da78dfcb1bec1df92b82d9
jgleason <jeffrey.gleason@newknowledge.io>
2019-05-03 16:19:34 -0500

    create own dictionary of resource metadata

--- D3MJoin/join.py
+++ D3MJoin/join.py
@@ -161,12 +161,10 @@ class Join(transformer.TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
         semantic_types_right = semantic_types_right.sample(frac=self.hyperparams['sample_size'], random_state = self.random_seed)
 
         logging.debug('Adding resource metadata back to processed dataset')
-        resource_map_left = left.metadata.query((left_resource_id, metadata_base.ALL_ELEMENTS))
-        resource_map_left[left_resource_id] = semantic_types_left
-        resource_map_right = left.metadata.query((left_resource_id, metadata_base.ALL_ELEMENTS))
-        resource_map_right[right_resource_id] = semantic_types_right
-        semantic_types_left = container.Dataset(resource_map_left)
-        semantic_types_right = container.Dataset(resource_map_right)
+        semantic_types_left = container.Dataset({'resource_id':semantic_types_left,
+                        'dimension':{'length': left.metadata.query((left_resource_id, metadata_base.ALL_ELEMENTS))['dimension']['length']}})
+        semantic_types_right = container.Dataset({'resource_id':semantic_types_right,
+                        'dimension':{'length': right.metadata.query((right_resource_id, metadata_base.ALL_ELEMENTS))['dimension']['length']}})
 
         logging.debug('Checking for first order semantic types matches')
         result = self._compare_results( \
